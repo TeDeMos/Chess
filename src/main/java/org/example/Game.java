420dd0140c44da5b01a0fc5ed805065b8bfe35de
Michał Kozłowski <01178281@pw.edu.pl>
2023-06-15 19:42:44 +0200

    stalemate bugfix

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -108,7 +108,7 @@ public class Game {
     /**
      * @return true if previous player has done a checkmate in their last move
      */
-    public Player isCheckMate(){
+    public Player isCheckMate() {
         Player checked;
         if (isCheck(player1)) checked = player1;
         else if (isCheck(player2)) checked = player2;
@@ -135,27 +135,17 @@ public class Game {
             staleMate = true;
             return true;
         }
-        if (whoseMove == player1) {
-            for (int i = 0; i < 8; i++) {
-                for (int y = 0; y < 8; y++) {
-                    if (board.getSquare(i, y).getPiece().getColour() == Colour.WHITE) {
-                        if (board.getSquare(i, y).getPiece().getPossibleMoves().size() != 0) {
-                            return false;
-                        }
-                    }
-                }
-            }
-        } else {
-            for (int i = 0; i < 8; i++) {
-                for (int y = 0; y < 8; y++) {
-                    if (board.getSquare(i, y).getPiece().getColour() == Colour.BLACK) {
-                        if (board.getSquare(i, y).getPiece().getPossibleMoves().size() != 0) {
-                            return false;
-                        }
-                    }
+        Colour colour = whoseMove.getColour();
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                if (board.getSquare(x, y).getPiece() != null
+                        && board.getSquare(x, y).getPiece().getColour() == colour
+                        && !board.getSquare(x, y).getPiece().getPossibleMoves().isEmpty()) {
+                    return false;
                 }
             }
         }
+        staleMate = true;
         return true;
     }
 
